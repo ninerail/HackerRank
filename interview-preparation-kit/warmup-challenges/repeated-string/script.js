4a6f12b941bf8d3d202164833e85b58f1aa2f55e
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-10-08 21:48:50 -0500

    completing-repeated-string-3

--- interview-preparation-kit/warmup-challenges/repeated-string/script.js
+++ interview-preparation-kit/warmup-challenges/repeated-string/script.js
@@ -1,7 +1,7 @@
 //Test values
-var n = 10;
-var s = "aba";
-var goal = 7;
+var n = 649606239668;
+var s = "bcbccacaacbbacabcabccacbccbababbbbabcccbbcbcaccababccbcbcaabbbaabbcaabbbbbbabcbcbbcaccbccaabacbbacbc";
+var goal = 162401559918;
 var actual = repeatedString(s, n);
 
 //Update html
@@ -15,12 +15,26 @@ function repeatedString(s, n) {
   if (s.length === 1 && s === "a") return n;
   let numberAs = 0;
   let charactersCounted = 0;
-  do {
-    for (let j = 0; j < s.length; j++){
-      charactersCounted++;
-      if (s.charAt(j) === "a") numberAs++;
-      if (charactersCounted === n) break;
-    }  
-  } while (charactersCounted < n);
+  for (let j = 0; j < s.length; j++){ // loop over string once and count the a's
+    if (s.charAt(j) === "a") numberAs++;
+  }
+  let repeats = n / s.length;
+  console.log('repeats ' + repeats);
+  let wholeRepeats = Math.floor(repeats); //calculate number of times whole string repeats
+  console.log('wholeRepeats ' + wholeRepeats);
+  numberAs = wholeRepeats * numberAs;
+  let remainderLength = n - (s.length * wholeRepeats);
+  console.log('remainderLength ' + remainderLength);
+  for (let j = 0; j < remainderLength; j++){
+    if (s.charAt(j) === "a") numberAs++;
+  }
+  // APPROACH BELOW WAS TOO TIME CONSUMING
+  // do {
+  //   for (let j = 0; j < s.length; j++){
+  //     charactersCounted++;
+  //     if (s.charAt(j) === "a") numberAs++;
+  //     if (charactersCounted === n) break;
+  //   }  
+  // } while (charactersCounted < n);
   return numberAs;
 }
