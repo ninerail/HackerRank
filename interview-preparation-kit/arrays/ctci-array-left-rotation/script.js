302e0719a4f3c72351f46556b8d1af2fa5d2e76f
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-10-09 08:28:27 -0500

    setting-up-ctci-left-rotation

---
+++ interview-preparation-kit/arrays/ctci-array-left-rotation/script.js
@@ -0,0 +1,16 @@
+//Test values
+var n = 9;
+var x = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+var goal = 3;
+var actual = myFunction(n, ar);
+
+//Update html
+document.getElementById("n").innerHTML = n;
+document.getElementById("x").innerHTML = x;
+document.getElementById("goal").innerHTML = goal;
+document.getElementById("actual").innerHTML = actual;
+
+// Core function
+function myFunction(n, x) {
+ 
+}
