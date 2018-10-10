988bb08e3a4bced780f1f12b65c78def9d1949c4
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-10-10 07:13:41 -0500

    ctci-array-left-rotation-writing-function

--- interview-preparation-kit/arrays/ctci-array-left-rotation/script.js
+++ interview-preparation-kit/arrays/ctci-array-left-rotation/script.js
@@ -3,7 +3,7 @@ var n = 5;
 var d = 4;
 var a = [1, 2, 3, 4, 5];
 var goal = [5, 1, 2, 3, 4];
-// var actual = myFunction(n, ar);
+var actual = rotLeft(a, d);
 
 //Update html
 document.getElementById("n").innerHTML = n;
@@ -13,8 +13,8 @@ document.getElementById("goal").innerHTML = goal;
 document.getElementById("actual").innerHTML = actual;
 
 // Core function
-function myFunction(n, x) {
- 
+function rotLeft(a, d) {
+  console.log(a, d);
+
 }
 
-console.log("hi mom");
